cd22ffe0928d9e1d5b750b7b4922cb3ea63db8a3
JawadJisan <jisan.sc@gmail.com>
2022-04-18 09:00:02 +0600

    services image and description added

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -12,7 +12,7 @@ const Checkout = () => {
             <h1 className=' text-center mt-5 font-bold text-3xl text-sky-700'>Congrats for Choosing The : <span className='text-red-900 text-4xl'>{name}</span> service</h1>
 
             <p className=' text-center mt-5 font-bold text-3xl text-rose-600'>Please Fil up this Form 😃</p>
-            <p className='text-center mt-3 mb-5 text-2xl'>Let me know what you think about me services </p>
+            <p className='text-center mt-3 mb-5 text-2xl'>Let me know what you think about my services </p>
         
         <HookForm></HookForm>
         {/* <App></App> */}
